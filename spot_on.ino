c90061fc3d043c8d679df73ac6ec6f5ff66858ba
Michael Liston <michaelowen.liston@gmail.com>
2015-07-01 21:56:10 +0200

    Cleaning this up, removing example code

--- spot_on.ino
+++ spot_on.ino
@@ -1,16 +1,95 @@
-//spot_on.ino
+// Using liberal bits of the following code:
+/*
+  MP3 Shield Trigger
+  by: Jim Lindblom
+      SparkFun Electronics
+  date: September 23, 2013
 
-int led = 13;
+  Much of this code was grabbed from the FilePlayer example
+  included with the SFEMP3Shield library. Major thanks to Bill
+  Porter and Michael Flaga, again, for this amazing library!
+*/
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(led, OUTPUT);
+#include <SPI.h>           // SPI library
+#include <SdFat.h>         // SDFat Library
+#include <SdFatUtil.h>     // SDFat Util Library
+#include <SFEMP3Shield.h>  // Mp3 Shield Library
+
+SdFat sd; // Create object to handle SD functions
+
+SFEMP3Shield MP3player; // Create Mp3 library object
+// These variables are used in the MP3 initialization to set up
+// some stereo options:
+uint8_t volume = 0; // MP3 Player volume 0=max, 255=lowest (off)
+const uint16_t monoMode = 1;  // Mono setting 0=off, 3=max
+
+/* Pin setup */
+
+
+// Magnet stuff
+#define hall A0
+int sensorVal;
+bool playState = false;
+
+void setup()
+{
+  Serial.begin(9600);
+
+
+  initSD();  // Initialize the SD card
+  initMP3Player(); // Initialize the MP3 Shield
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-    digitalWrite(led, HIGH);  // turn the LED on (HIGH is the voltage level)
-    delay(100);               // wait for a second
-    digitalWrite(led, LOW);   // turn the LED off by making the voltage LOW
-    delay(100);               // wait for a second
+void loop()
+{
+  sensorVal = analogRead(hall);
+  // Serial.println(sensorVal);
+
+  if (sensorVal < 200){
+    playState = true;
+
+    if (MP3player.isPlaying() == false){
+      uint8_t result = MP3player.playTrack(1);
+    }
+  } else {
+    playState = false;
+    if (MP3player.isPlaying()){
+      MP3player.stopTrack();
+    }
+  }
+
+
+  Serial.print("playState: ");
+  Serial.println(playState);
+
+  Serial.print("isPlaying(): ");
+  Serial.println(MP3player.isPlaying());
+
+
 }
+
+// initSD() initializes the SD card and checks for an error.
+void initSD()
+{
+  //Initialize the SdCard.
+  if(!sd.begin(SD_SEL, SPI_HALF_SPEED)) 
+    sd.initErrorHalt();
+  if(!sd.chdir("/")) 
+    sd.errorHalt("sd.chdir");
+}
+
+// initMP3Player() sets up all of the initialization for the
+// MP3 Player Shield. It runs the begin() function, checks
+// for errors, applies a patch if found, and sets the volume/
+// stero mode.
+void initMP3Player()
+{
+  uint8_t result = MP3player.begin(); // init the mp3 player shield
+  if(result != 0) // check result, see readme for error codes.
+  {
+    // Error checking can go here!
+  }
+  MP3player.setVolume(volume, volume);
+  MP3player.setMonoMode(monoMode);
+}
+
